05c82eeb551870e488046830a109af000438b06a
temple3fky-hue <temple3fky@gmail.com>
2026-04-28 00:56:54 +0800

    Add Gradle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WatchChat"
+include(":mobile-app")
+include(":wear-app")
+include(":shared")
