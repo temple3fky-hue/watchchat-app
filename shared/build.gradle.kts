c602fffa55a990329cb3504c12758551128c67f8
temple3fky-hue <temple3fky@gmail.com>
2026-04-28 01:00:26 +0800

    Add shared module build file

---
+++ shared/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.temple.watchchat.shared"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
