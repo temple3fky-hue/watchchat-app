e658885b87b29c02ed049ae8cf2e511c14279c59
temple3fky-hue <temple3fky@gmail.com>
2026-04-28 02:29:00 +0800

    Enable serialization in shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -10,4 +11,8 @@ android {
     defaultConfig {
         minSdk = 26
     }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
