be9ab8c1848955773561e20aaeb69299ecb0681d
temple3fky-hue <temple3fky@gmail.com>
2026-04-28 02:29:31 +0800

    Make shared user model serializable

--- shared/src/main/java/com/temple/watchchat/shared/model/WatchChatUser.kt
+++ shared/src/main/java/com/temple/watchchat/shared/model/WatchChatUser.kt
@@ -1,11 +1,14 @@
 package com.temple.watchchat.shared.model
 
+import kotlinx.serialization.Serializable
+
 /**
  * WatchChat 用户基础信息。
  *
  * 第一版先只保存聊天展示需要的字段。
  * 后续接入 Supabase Auth 后，id 可以对应 auth.users.id。
  */
+@Serializable
 data class WatchChatUser(
     val id: String,
     val email: String,
